d0ac623dd6fe1fa9fda7c4de60360f19e33dd79e
asmartynyshyn <85318898+asmartynyshyn@users.noreply.github.com>
2023-05-11 14:32:57 -0400

    Add_Button_Icon_Basket

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,11 +3,13 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE, BASKET_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
-import {useHistory} from 'react-router-dom'
+import {useHistory} from 'react-router-dom';
+import {FiShoppingCart} from 'react-icons/fi' ;
+
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     const history = useHistory()
@@ -25,9 +27,18 @@ const NavBar = observer(() => {
                     <Nav className="ml-auto" style={{color: 'white'}}>
                         <div style={{padding: "5px 25px 0 0", fontSize: "20px"}}>{user.user}</div>
                         
+                        <Button 
+                            variant={"outline-light"}
+                            className="mr-2"
+                            onClick = {()=> history.push(BASKET_ROUTE)}
+                        >
+                            <FiShoppingCart />
+                        </Button>
+
                         {user.role === "ADMIN" &&
                             <Button
                                 variant={"outline-light"}
+                                className="mr-2"
                                 onClick={() => history.push(ADMIN_ROUTE)}
                             >
                                 Admin
@@ -37,7 +48,6 @@ const NavBar = observer(() => {
                         <Button
                             variant={"outline-light"}
                             onClick={() => logOut()}
-                            className="ml-2"
                         >
                             Exit
                         </Button>
